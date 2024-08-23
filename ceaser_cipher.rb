a975434cbd8feea1940d5b8a5f7a56ac62fe3ec6
bjornrohrer <barohrerdk@gmail.com>
2024-08-23 13:14:17 +0200

    Copied code from repl I made to test things out.

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,1 +1,28 @@
+class Ceaser
+  @@UPPERCASE_LETTERS = ('A'..'Z').to_a
+  @@LOWERCASE_LETTERS = ('a'..'z').to_a
+
+  attr_accessor = :key
+
+  def initialize(key)
+    @key = key
+  end
+
+  def encrypt(text)
+    encrypted = ''
+    text.split('').each do |x|
+      encrypted += if @@UPPERCASE_LETTERS.include? x
+                     @@UPPERCASE_LETTERS [(@@UPPERCASE_LETTERS.index(x) + @key) % 26]
+                   elsif @@LOWERCASE_LETTERS.include? x
+                     @@LOWERCASE_LETTERS [(@@LOWERCASE_LETTERS.index(x) + @key) % 26]
+                   else
+                     x
+                   end
+    end
+    encrypted
+  end
+end
+
+cipher = Ceaser.new(5)
+puts cipher.encrypt('fuck you')
 
