c2c28d3095be3f974d33a51217228386385622c3
bjornrohrer <barohrerdk@gmail.com>
2024-08-23 13:21:19 +0200

    Added an else statement.

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,22 +1,23 @@
 class Ceaser
-  @@UPPERCASE_LETTERS = ('A'..'Z').to_a
-  @@LOWERCASE_LETTERS = ('a'..'z').to_a
+  @@UPPERCASE_LETTERS = ('A'..'Z').to_a #Creates an array of letters from A to Z in uppercase.
+  @@LOWERCASE_LETTERS = ('a'..'z').to_a #Creates an array of letters from A to Z in lowercase.
 
-  attr_accessor = :key
+  attr_accessor = :key #Gets and sets the key without using methods.
 
-  def initialize(key)
-    @key = key
+  def initialize(key) 
+    #Constructor recevies the key upon initailization. 
+    @key = key #Made the key accesible within a variable. 
   end
 
   def encrypt(text)
-    encrypted = ''
-    text.split('').each do |x|
-      encrypted += if @@UPPERCASE_LETTERS.include? x
-                     @@UPPERCASE_LETTERS [(@@UPPERCASE_LETTERS.index(x) + @key) % 26]
+    encrypted = '' #Variable that holds the encrypted text. 
+    text.split('').each do |x| #splits the given text into an array and iterates over each index inside the array. 
+      encrypted += if @@UPPERCASE_LETTERS.include? x #Checks if any uppercase letter is included in the variable. 
+                     @@UPPERCASE_LETTERS [(@@UPPERCASE_LETTERS.index(x) + @key) % 26] #Finds the index of the variable and adds the key to it. 
                    elsif @@LOWERCASE_LETTERS.include? x
-                     @@LOWERCASE_LETTERS [(@@LOWERCASE_LETTERS.index(x) + @key) % 26]
+                     @@LOWERCASE_LETTERS [(@@LOWERCASE_LETTERS.index(x) + @key) % 26] #Finds the index of the variable and adds the key to it
                    else
-                     x
+                     encrypted += x
                    end
     end
     encrypted
