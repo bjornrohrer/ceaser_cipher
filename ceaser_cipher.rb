b74e8055b6e5003c4c11d6a246d88bc9421852e1
bjornrohrer <barohrerdk@gmail.com>
2024-08-26 12:34:36 +0200

    Made the file more readable with the help of rubocop

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,29 +1,30 @@
 class Ceaser
-  @@UPPERCASE_LETTERS = ('A'..'Z').to_a #Creates an array of letters from A to Z in uppercase.
-  @@LOWERCASE_LETTERS = ('a'..'z').to_a #Creates an array of letters from A to Z in lowercase.
+  @@UPPERCASE_LETTERS = ('A'..'Z').to_a # Creates an array of letters from A to Z in uppercase.
+  @@LOWERCASE_LETTERS = ('a'..'z').to_a # Creates an array of letters from A to Z in lowercase.
 
-  attr_accessor = :key #Gets and sets the key without using methods.
+  attr_accessor = :key # Gets and sets the key without using methods.
 
-  def initialize(key) 
-    #Constructor recevies the key upon initailization. 
-    @key = key #Made the key accesible within a variable. 
+  def initialize(key)
+    # Constructor recevies the key upon initailization.
+    @key = key # Made the key accesible within a variable.
   end
 
   def encrypt(text)
-    encrypted = '' #Variable that holds the encrypted text. 
-    text.split('').each do |x| #splits the given text into an array and iterates over each index inside the array. 
-      encrypted += if @@UPPERCASE_LETTERS.include? x #Checks if any uppercase letter is included in the variable. 
-                     @@UPPERCASE_LETTERS [(@@UPPERCASE_LETTERS.index(x) + @key) % 26] #Finds the index of the variable and adds the key to it. 
+    encrypted = '' # Variable that holds the encrypted text.
+    text.split('').each do |x| # splits the given text into an array and iterates over each index inside the array.
+      encrypted += if @@UPPERCASE_LETTERS.include? x # Checks if any uppercase letter is included in the variable.
+                     @@UPPERCASE_LETTERS [(@@UPPERCASE_LETTERS.index(x) + @key) % 26] # Finds the index of the variable,
+                     # and adds the key to it.
                    elsif @@LOWERCASE_LETTERS.include? x
-                     @@LOWERCASE_LETTERS [(@@LOWERCASE_LETTERS.index(x) + @key) % 26] #Finds the index of the variable and adds the key to it
+                     @@LOWERCASE_LETTERS [(@@LOWERCASE_LETTERS.index(x) + @key) % 26] # Finds the index of the variable,
+                     # and adds the key to it
                    else
                      x
                    end
     end
-    encrypted #Returns the enctrypted text. 
+    encrypted # Returns the enctrypted text.
   end
 end
 
-cipher = Ceaser.new(5) #Calls the method 
+cipher = Ceaser.new(5) # Calls the method
 puts cipher.encrypt('Hello World')
-
